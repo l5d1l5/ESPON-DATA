380ced0d69215ac71026598170756b80cb4f35a7
mohamed-hachaichi <hachaichi_mohamed@outlook.com>
2022-01-06 18:52:13 +0100

    fix: cases & deaths & GIS

---
+++ 01_Code/02_Analysis/Analysis.R
@@ -0,0 +1,16 @@
+
+## Using hierarchical clustring with 
+## soft contiguity constraints 
+
+------------------
+# Read packages 
+
+library(tidyvers)
+library(ClustGeo)
+
+
+-------------
+# Read the data 
+
+
+
